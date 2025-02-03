0e6c11e49236d39c6d62d3a15305e3f14a74a56e
JustinLin0121 <157280507+JustinLin0121@users.noreply.github.com>
2025-02-03 07:57:19 +0000

    feat: make it look prettier add background image and some code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
-    // 判断侧边栏当前位置，如果在屏幕外则移入，反之移出
     if (sidebar.style.left != '0px') {
         sidebar.style.left = '0px'; //顯示
     } else {
-        sidebar.style.left = '-16vw';//隱藏
+        sidebar.style.left = '-16.5vw';//隱藏
     }
 }
