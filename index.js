7c798f589343d757cf2ee88d764592dc2c3a806a
JustinLin0121 <157280507+JustinLin0121@users.noreply.github.com>
2025-02-03 07:02:23 +0000

    feat:add some code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function toggleSidebar() {
-    var sidebar = document.querySelector(".sidebar");
-    if (sidebar.style.display === "none" || sidebar.style.display === "") {
-        sidebar.style.display = "flex";
+    const sidebar = document.querySelector('.sidebar');
+    // 判断侧边栏当前位置，如果在屏幕外则移入，反之移出
+    if (sidebar.style.left != '0px') {
+        sidebar.style.left = '0px'; //顯示
     } else {
-        sidebar.style.display = "none";
+        sidebar.style.left = '-16vw';//隱藏
     }
 }
