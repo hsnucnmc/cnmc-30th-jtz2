c12acde056427c64fc420d6c3ff1a63e68e8dd35
JustinLin0121 <157280507+JustinLin0121@users.noreply.github.com>
2025-02-05 08:22:06 +0000

    ffdfd

--- decl.js
+++ decl.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pElements = document.querySelectorAll('.word p');
 
-    pElements.forEach((p) => {
-        p.addEventListener('mouseenter', () => {
-            pElements.forEach((otherp) => {
-                if (otherp !== p) {
-                    otherp.classList.add('dimmed'); // 其他元素變暗
-                }
-            });
-        });
-
-        h2.addEventListener('mouseleave', () => {
-            pElements.forEach((otherp) => {
-                otherp.classList.remove('dimmed'); // 恢復正常
-            });
-        });
-    });
-});
